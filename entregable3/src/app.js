b7052266bb6eb45c8b68dab15833986d75c01a0b
SteveO <orregoe@gmail.com>
2023-03-13 23:40:30 -0300

    clase de motores de plantilla

--- entregable3/src/app.js
+++ entregable3/src/app.js
@@ -7,7 +7,10 @@ import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
 // importar el __dirname para poder usarlo en app.js
-import {__dirname} from './utils/dirname.js';
+import { __dirname } from './utils/dirname.js';
+
+// importar el modulo de handlebars para poder usarlo en app.js y en la carpeta views
+import handlebars from 'express-handlebars';
 
 //crear una aplicación express
 const app = express();
@@ -19,12 +22,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // los archivos estáticos (HTML, CSS, JS) se sirven desde la carpeta pública (public)
-app.use('/public',express.static(__dirname));
+app.use('/public', express.static(__dirname + '/public/html'));
 //las rutas para los endpoints de la API de productos (REST) se definen en el router de productos (products.router.js) y se asignan a la ruta /api/products
 app.use('/api/products', productsRouter);
 
 //las rutas para los endpoints de la API de carritos (REST) se definen en el router de carritos (carts.router.js) y se asignan a la ruta /api/carts
 app.use('/api/carts', cartsRouter);
+
+// configurar el motor de plantillas (template engine) para que use handlebars
+app.engine('handlebars', handlebars.engine());
+
+// configurar la carpeta de vistas (views) y el motor de plantillas (template engine) para que use handlebars
+app.set('views', __dirname + '/views');
+
+// configurar el motor de plantillas (template engine) para que use handlebars
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('primera');
+});
+
+app.get('/segunda', (req, res) => {
+    res.render('segunda');
+});
 
 
 // escuchar en el puerto 8080 y mostrar un mensaje en la consola cuando el servidor esté inicializado (listening)
