ef3f0e84174c37f571b754d24336c1b7cab26928
SteveO <orregoe@gmail.com>
2023-04-10 21:02:28 -0300

    modifico credenciales

--- desafio_complementario/src/db/dbConfig.js
+++ desafio_complementario/src/db/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const URI = 'mongodb+srv://orregoe:2zXj2c3c0VkNNsiR@steveo.bxgkikt.mongodb.net/ecommerce?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://desafioComplementario:coderHouse@steveo.bxgkikt.mongodb.net/ecommerce?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
     .then(() => console.log('DB is connected'))
